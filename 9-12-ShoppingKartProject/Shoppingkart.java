3643e8ad45ab5cd242cc36cbd9028ebc0c8e7595
swastikamishra <115781825+swastikamishra@users.noreply.github.com>
2022-12-10 01:21:18 +0530

    Add files via upload

---
+++ 9-12-ShoppingKartProject/Shoppingkart.java
@@ -0,0 +1,13 @@
+package ShoppingKartProject1;
+
+//This class is the main entry point 
+
+public class Shoppingkart 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t~~ Welcome To Shopping Kart ~~");
+		ShoppingKartOperations.AllMenus();
+
+	}
+}
