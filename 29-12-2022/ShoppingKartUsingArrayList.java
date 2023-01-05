1237fe78bbd3295e542f955cc4b3d180159a0dbc
swastikamishra <115781825+swastikamishra@users.noreply.github.com>
2023-01-05 15:22:40 +0530

    Add files via upload

---
+++ 29-12-2022/ShoppingKartUsingArrayList.java
@@ -0,0 +1,11 @@
+package ThursdayLabNew;
+
+public class ShoppingKartUsingArrayList {
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t~~ Welcome To Shopping Kart ~~");
+		ShoppingKartArrayListOperations.mainMenu();
+	}
+
+}
